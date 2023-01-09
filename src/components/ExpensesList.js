d0eb68b12bff0590a7631c5b50e71802b042cdc6
zohaib <zohaib.s169@gmail.com>
2023-01-09 16:56:26 +0500

    new file added

---
+++ src/components/ExpensesList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './ExpensesList.css'
+function ExpensesList (props){
+    return (
+        <ul className='expenses-list'>
+
+        </ul>
+    );
+}
+export default ExpensesList;
