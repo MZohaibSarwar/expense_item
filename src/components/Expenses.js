9c328ed7b6c59a7163f063b4c45cef8dd151fc8d
zohaib <zohaib.s169@gmail.com>
2023-01-13 10:04:52 +0500

    project completed

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -2,6 +2,8 @@ import React, {useState} from "react";
 import './Expenses.css';
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 function Expenses(props) {
 
 
@@ -13,23 +15,14 @@ function Expenses(props) {
             return expense.date.getFullYear().toString() === filteredYear;
          });
 
-         let expensesContent = <p>No expenses found!</p>;
-         if(filteredExpenses.length > 0){
-             expensesContent = filteredExpenses.map((expense) => (
-                    <ExpenseItem
-                    key={expense.id}
-                    title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date}
-                />
-                ));
-         }
+
 
         return(
             <div className="expenses">
 
                  <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                 {expensesContent}
+                <ExpensesChart expenses={filteredExpenses}/>
+                 <ExpensesList items={filteredExpenses}/>
 
             </div>
 );
